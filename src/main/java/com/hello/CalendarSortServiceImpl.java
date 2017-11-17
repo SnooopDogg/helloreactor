e032e336acf2a352eb1b350f0b5de525b4a981ab
Mykhaylo_Gnylorybov <Mykhaylo_Gnylorybov@epam.com>
2017-11-17 12:40:24 +0200

    parralel sort

--- src/main/java/com/hello/CalendarSortServiceImpl.java
+++ src/main/java/com/hello/CalendarSortServiceImpl.java
@@ -1,9 +1,11 @@
 package com.hello;
 
 import java.time.Duration;
-import java.util.List;
-import java.util.stream.Collectors;
+import java.util.ArrayList;
+import java.util.Collections;
 
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
 import org.springframework.stereotype.Service;
 
 import reactor.core.publisher.Flux;
@@ -11,30 +13,37 @@ import reactor.core.publisher.Flux;
 @Service
 public class CalendarSortServiceImpl implements CalendarSortService {
 
+    private static final int SLEEP_DELAY = 50;
+
     private static final Duration MONTH_SORT_DELAY = Duration.ofMillis(1000);
     private static final Duration WEEK_DAY_SORT_DELAY = Duration.ofMillis(500);
+    private final Log logger = LogFactory.getLog(this.getClass());
 
     @Override
     public CalendarDto sort(final CalendarDto calendarDto) {
         try {
             final Flux<String> month = Flux.fromIterable(calendarDto.getMonths());
             final Flux<String> weekDay = Flux.fromIterable(calendarDto.getWeekDay());
+            final int totalMonth = calendarDto.getMonths().size();
+            final int totalWeekDays = calendarDto.getWeekDay().size();
 
             final CalendarDto result = new CalendarDto();
-            result.setMonths(fluxToString(sortAsync(month, MONTH_SORT_DELAY)));
-            result.setWeekDay(fluxToString(sortAsync(weekDay, WEEK_DAY_SORT_DELAY)));
+            result.setMonths(Collections.synchronizedList(new ArrayList<>(totalMonth)));
+            result.setWeekDay(Collections.synchronizedList(new ArrayList<>(totalWeekDays)));
+
+            month.sort().delayElements(MONTH_SORT_DELAY).subscribe(result.getMonths()::add);
+            weekDay.sort().delayElements(WEEK_DAY_SORT_DELAY).subscribe(result.getWeekDay()::add);
+
+            while (totalMonth > result.getMonths().size() || totalWeekDays > result.getWeekDay().size()) {
+                try {
+                    Thread.sleep(SLEEP_DELAY);
+                } catch (InterruptedException e) {
+                    logger.error("Main thread interrupted", e);
+                }
+            }
             return result;
         } catch (NullPointerException e) {
             throw new SortException(e);
         }
     }
-
-    private Flux<String> sortAsync(final Flux<String> stringFlux, final Duration delay) {
-        return stringFlux.delayElements(delay).sort();
-    }
-
-    private List<String> fluxToString(final Flux<String> stringFlux) {
-        return stringFlux.toStream().collect(Collectors.toList());
-    }
-
 }
